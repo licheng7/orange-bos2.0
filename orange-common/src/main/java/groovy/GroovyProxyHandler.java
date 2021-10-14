2c6c9617ba2fa562ac2a60adbfabcb95a0309af0
licheng <1031751607@qq.com>
2021-10-15 02:16:14 +0800

    commit for common-groovy

---
+++ orange-common/src/main/java/groovy/GroovyProxyHandler.java
@@ -0,0 +1,27 @@
+/**
+ * ClassName: GroovyProxyHandler
+ * Description:
+ * date: 2021/10/15 1:34 上午
+ *
+ * @author licheng
+ */
+package groovy;
+
+import groovy.lang.GroovyObject;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class GroovyProxyHandler implements InvocationHandler {
+
+    private long id;
+
+    public GroovyProxyHandler(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return GroovyExecutor.getInstance().execute(id, method);
+    }
+}
